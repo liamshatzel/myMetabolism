7a0f2225d3d6c4695733da68746a2e9882f69389
liamshatzel <liam.shatzel@gmail.com>
2021-07-16 11:18:18 -0700

    Initial Commit

---
+++ MyMetabolism/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMetabolism
+//
+//  Created by Liam Shatzel on 7/16/21.
+//  Copyright © 2021 Liam Shatzel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
