ffc2a237d5e47c61f043204eb967d55c81fe7779
liamshatzel <liam.shatzel@gmail.com>
2021-07-16 12:09:14 -0700

    first commit

---
+++ MyMetabolism/View Controllers/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  MyMetabolism
+//
+//  Created by Liam Shatzel on 7/16/21.
+//  Copyright © 2021 Liam Shatzel. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
